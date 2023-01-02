db3c72ed088d65f0c41d8b770215204dd27873a3
abayasaye <abay400@gmail.com>
2023-01-02 15:47:29 +0200

    push

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,13 +1,11 @@
-const ordersModel = require("../models/orders")
+const ordersModel = require("../models/orders");
 
-const getAll = async (req, res)=>{
-    await ordersModel.find({})
-    .then((result, err)=>{
-        if(err){
-            res.status(400).json({success: false, message: err})
-        }
-        else{
-            res.status(200).json({success:true, message: result})
-        }
-    })
-}
+const getAll = async (req, res) => {
+  await ordersModel.find({}).then((result, err) => {
+    if (err) {
+      res.status(400).json({ success: false, message: err });
+    } else {
+      res.status(200).json({ success: true, message: result });
+    }
+  });
+};
