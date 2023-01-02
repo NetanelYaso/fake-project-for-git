f816bba4473bbb87ba8cc191cfd5f68257ab70c7
chen-semu <chensemu@gmail.com>
2023-01-02 16:58:21 +0200

    dfdf

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -15,3 +15,20 @@ router.put("/update", update);
 router.delete("/delete", deleteUser)
 
 module.exports = router;
+// const router = require("express").Router();
+// const {
+//     getAll,
+//     getById,
+//     create,
+//     update,
+//     deleteOrder
+// } = require("../controllers/orders");
+
+
+// router.get("/", getAll);
+// router.get("/byId", getById);
+// router.post("/saveData", create);
+// router.put("/update", update);
+// router.delete("/delete", deleteOrder)
+
+// module.exports = router;
