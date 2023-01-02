fcb8e01a37d73544f2f6e42c84203773565c729d
NetanelYaso <netanelyaso12@gmail.com>
2023-01-02 21:50:42 +0200

    zdxc

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
-const app = express();
 
 
 app.use(express.json({ extened: true }));
