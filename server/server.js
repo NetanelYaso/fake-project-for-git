f0ab6097015eb7e059df47b505a91fd3ffffefa3
chen-semu <chensemu@gmail.com>
2023-01-04 14:46:18 +0200

    fixing dev branch

--- server/server.js
+++ server/server.js
@@ -4,10 +4,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8080;
-
-app.use(express.json({ extened: true }));
+require("./DB")
+app.use(express.json({ exteneded: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+const usersRouter=require("./routes/usersRoute")
+const departmentsRouter=require("./routes/departmentsRoute")
+const reviewsRouter=require("./routes/reviewsRoute")
+const ordersRouter=require("./routes/ordersRoute")
+const lawyersRouter=require("./routes/lawyersRoute")
+
+app.use("/users",usersRouter)
+app.use("/departments",departmentsRouter)
+app.use("/reviews",reviewsRouter)
+app.use("/orders",ordersRouter)
+app.use("/lawyers",lawyersRouter)
+
 
 app.get("/", (req, res) => {
     res.send({ message: "ayo whatsup" });
